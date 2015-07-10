74b2b8862908bb520346ac19f116e1b902653cda
Hugo Abonizio <hugo_abonizio@hotmail.com>
2015-07-10 00:17:16 +0000

    Implement render method in controller

--- spec/controller_spec.cr
+++ spec/controller_spec.cr
@@ -2,6 +2,13 @@ require "./spec_helper"
 require "http"
 
 class MyController < Banjo::Controller::Base
+  def test_text
+    render text: "Hello, World!"
+  end
+  
+  def test_html
+    render html: "<h1>Hello, World!</h1>"
+  end
 end
 
 describe Banjo::Controller::Base do
@@ -10,5 +17,17 @@ describe Banjo::Controller::Base do
     context = Banjo::Context.new(request)
     controller = MyController.new(context)
     controller.params["p1"].should eq("666")
+  end
+  
+  it "should render inline" do
+    controller = MyController.new(Banjo::Context.new)
+    
+    controller.test_text
+    controller.response.body.should eq "Hello, World!"
+    controller.response.headers["Content-type"].should eq "text/plain"
+    
+    controller.test_html
+    controller.response.body.should eq "<h1>Hello, World!</h1>"
+    controller.response.headers["Content-type"].should eq "text/html"
   end
 end
--- src/banjo/controller.cr
+++ src/banjo/controller.cr
@@ -1,11 +1,22 @@
 module Banjo
   module Controller
     class Base
+      getter response
+      
       def initialize(@context = Banjo::Context.new)
+        @response :: HTTP::Response
       end
       
       def params
         @context.params
+      end
+      
+      def render(text = nil, html =  nil)
+        if !text.nil?
+          @response = HTTP::Response.ok("text/plain", text)
+        elsif !html.nil?
+          @response = HTTP::Response.ok("text/html", html)
+        end
       end
     end
   end
